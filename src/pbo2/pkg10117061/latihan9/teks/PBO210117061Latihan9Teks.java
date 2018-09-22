884e2873be86b2d159d1eb51789d820c5269f020
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-09-22 19:12:08 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117061/latihan9/teks/PBO210117061Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan9.teks;
+
+/**
+ *
+ * @author Fachrian Harits Pratama
+ * Nama                 : Fachrian Harits Pratama
+ * Kelas                : PBO2
+ * NIM                  : 10117061
+ * Deskripsi program    : Program ini berisi program  menampilkan jenis-jenis
+ *                        tipe data teks
+ */
+public class PBO210117061Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("hasil" + newline + "char: " + tab + "Berisi " + c);
+        
+    }
+    
+}
